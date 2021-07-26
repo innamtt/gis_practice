21cccc8edf8dde0af4feba12f417e3f1f39cb6a9
innamtt <innamtt@gmail.com>
2021-07-26 11:30:17 +0900

    2021-07-26 1st Commit

--- accountapp/models.py
+++ accountapp/models.py
@@ -1,6 +1,9 @@
+from django.contrib.auth.models import User
 from django.db import models
-
 # Create your models here.
+
 
 class HelloWorld(models.Model):
     text = models.CharField(max_length=255, null=False)
+
+
