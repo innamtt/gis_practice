b3057e00af30f5c71a5b333a0258b8a737e547e2
innamtt <innamtt@gmail.com>
2021-07-29 11:58:24 +0900

    2021-07-29 2nd Commit

--- accountapp/decorators.py
+++ accountapp/decorators.py
@@ -10,4 +10,4 @@ def account_ownership_required(func):
             return func(request, *args, **kwargs)
         else:
             return HttpResponseForbidden()
-        return decorated
+    return decorated
