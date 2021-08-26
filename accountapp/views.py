9108f676ce39cba0409d474f42f8e37920a51209
innamtt <innamtt@gmail.com>
2021-08-26 12:45:45 +0900

    2021-08-26 4th Commit

--- accountapp/views.py
+++ accountapp/views.py
@@ -19,26 +19,6 @@ from accountapp.models import HelloWorld
 from articleapp.models import Article
 
 
-def hello_world(request):
-    if request.user.is_authenticated:
-        if request.method == "POST":
-
-            temp = request.POST.get('input_text')
-
-            new_hello_world = HelloWorld()
-            new_hello_world.text = temp
-            new_hello_world.save()
-
-            return HttpResponseRedirect(reverse('accountapp:hello_world'))
-
-        else:
-            hello_world_list = HelloWorld.objects.all()
-            return render(request, 'accountapp/hello_world.html',
-                          context={'hello_world_list': hello_world_list})
-    else:
-        return HttpResponseRedirect(reverse('accountapp:login'))
-
-
 class AccountCreateView(CreateView):
     model = User
     form_class = UserCreationForm
@@ -68,7 +48,7 @@ class AccountUpdateView(UpdateView):
     model = User
     form_class = AccountCreationForms
     context_object_name = 'target_user'
-    # success_url = reverse_lazy('accountapp:list')
+    # success_url = reverse_lazy('articleapp:list')
     template_name = 'accountapp/update.html'
 
     def get_success_url(self):
