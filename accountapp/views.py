895d5e913e56404ac7540254a56f5322247f0203
innamtt <innamtt@gmail.com>
2021-08-26 12:42:59 +0900

    2021-08-26 4th Commit

--- accountapp/views.py
+++ accountapp/views.py
@@ -42,7 +42,7 @@ def hello_world(request):
 class AccountCreateView(CreateView):
     model = User
     form_class = UserCreationForm
-    success_url = reverse_lazy('accountapp:hello_world')
+    success_url = reverse_lazy('articleapp:list')
     template_name = 'accountapp/create.html'
 
 
@@ -68,7 +68,7 @@ class AccountUpdateView(UpdateView):
     model = User
     form_class = AccountCreationForms
     context_object_name = 'target_user'
-    # success_url = reverse_lazy('accountapp:hello_world')
+    # success_url = reverse_lazy('accountapp:list')
     template_name = 'accountapp/update.html'
 
     def get_success_url(self):
@@ -80,5 +80,5 @@ class AccountUpdateView(UpdateView):
 class AccountDeleteView(DeleteView):
     model = User
     context_object_name = 'target_user'
-    success_url = reverse_lazy('accountapp:hello_world')
+    success_url = reverse_lazy('articleapp:list')
     template_name = 'accountapp/delete.html'
